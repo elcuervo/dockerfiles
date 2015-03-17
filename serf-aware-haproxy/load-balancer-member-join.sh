6e9880504220fa4c7f11a8ea5eecd364ea727bea
elCuervo <yo@brunoaguirre.com>
2015-03-17 13:12:49 -0300

    ENV var to change role to update

--- serf-aware-haproxy/load-balancer-member-join.sh
+++ serf-aware-haproxy/load-balancer-member-join.sh
@@ -7,7 +7,9 @@ fi
 
 ROLE=$(echo $line | awk '{print \$3 }')
 
-if [ "${ROLE}" == "web-node" ]; then
+[ -z "$UPDATE_ROLE" ] && UPDATE_ROLE="web-node"
+
+if [ $ROLE == $UPDATE_ROLE ]; then
   while read line; do
     echo $line | awk '{ printf "    server %s %s check\n", $1, $2 }' >>/etc/haproxy/haproxy.cfg
   done
