ebaee83ac17b2bf12eda7eacdaba6201b1a0af6c
elCuervo <yo@brunoaguirre.com>
2015-03-23 16:55:30 -0300

    Proper conf naming

--- consul-template/consul-template-loader.sh
+++ consul-template/consul-template-loader.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export GOMAXPROCS=$(nproc)
-consul-template -consul ${CONSUL_HOST} -config /etc/consul-template/templates/
+consul-template -consul ${CONSUL_HOST} -config /etc/consul-template/conf.d/
