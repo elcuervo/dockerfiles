e675f843f7d38c092dca5509a0af493009d37f0b
elCuervo <yo@brunoaguirre.com>
2015-03-24 17:04:27 -0300

    Do not start bootstrap by default

--- consul-supervisord/consul-agent.sh
+++ consul-supervisord/consul-agent.sh
@@ -3,7 +3,7 @@
 export GOMAXPROCS=$(nproc)
 FLAGS="-config-dir /etc/consul/consul.d -data-dir /tmp/consul"
 
-if [[ $CONSUL_CLIENT == false ]]; then
+if [[ $CONSUL_SERVER == true ]]; then
   FLAGS="$FLAGS -bootstrap-expect ${CONSUL_BOOTSTRAP_EXPECT:-1}"
 fi
 
