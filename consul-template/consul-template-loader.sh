453cbff994072018f977a429d0a3186576e4b648
elCuervo <yo@brunoaguirre.com>
2015-03-31 17:14:47 -0300

    Allow Supervisor to take control

--- consul-template/consul-template-loader.sh
+++ consul-template/consul-template-loader.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export GOMAXPROCS=$(nproc)
-consul-template -consul ${CONSUL_HOST}:${CONSUL_PORT:-8500} -config /etc/consul-template/conf.d/
+exec consul-template -consul ${CONSUL_HOST}:${CONSUL_PORT:-8500} -config /etc/consul-template/conf.d/
